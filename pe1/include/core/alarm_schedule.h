c9ef8304063e08dc1debd07e5d0690d709b605d6
Seran Shanmugathas <Seran@live.no>
2022-02-16 17:31:19 +0100

    :construction: Fixed path and adde common includes

--- pe1/include/core/alarm_schedule.h
+++ pe1/include/core/alarm_schedule.h
@@ -1,6 +1,7 @@
 #ifndef __ALARM_SCHEDULE_H
 #define _ALARM_SCHEDULE_H
 
-
+#include <stdio.h>
+#include <stdlib.h>
 
 #endif // _ALARM_SCHEDULE_H
---
+++ pe1/src/core/alarm_schedule.c
@@ -0,0 +1,2 @@
+#include "../../include/core/alarm_schedule.h"
+
