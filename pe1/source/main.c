ee04354522d83b4eaa2b6e74c8b4df6d2495be50
Francin Anoj Vincent <francinv@stud.ntnu.no>
2022-02-16 12:32:34 +0100

    (#1) Completed menu, with correct showing of time.

--- pe1/source/main.c
+++ pe1/source/main.c
@@ -2,29 +2,47 @@
 #include<time.h>
 #include<stdlib.h>
 
+char selected;
+
+//TODO - Complete this.
+/**
+ * @brief This is the method that will be called if user choses to schedule an alarm.
+ * 
+ */
 void schedule(){
     printf("You chose to schedule.\n");
 }
 
+//TODO - Complete this.
+/**
+ * @brief This is the method that will be called if user choses to list all alarms.
+ * 
+ */
 void list(){
     printf("You chose to display list.\n");
 }
 
+//TODO - Complete this.
+/**
+ * @brief This is the method that will be called if user choses to cancel an alarm.
+ * The user will also be prompted an input where the program will tell the user to 
+ * write the ID of the alarm to cancel.
+ * 
+ */
 void cancel(){
     printf("You chose to cancel an alarm.\n");
 }
 
+//TODO - Complete this.
+/**
+ * @brief This is the method that will be called if user choses to exit out of the program.
+ * 
+ */
 void optOut(){
-    printf("You chose to exit.\n");
+    printf("Goodbye!\n");
 }
 
-int main(){
-    time_t tm;
-    time(&tm);
-    printf("Welcome to the alarm clock! It is currently %s \n", ctime(&tm));
-    printf("Please enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit) \n");
-    char selected = getchar();
-    
+void checkInput(){
     switch (selected)
     {
     case 's':
@@ -43,5 +61,20 @@ int main(){
         printf("Nothing selected \n");
         break;
     }
+}
+
+//TODO - Not finished, will be updated.
+/**
+ * @brief This is the main method, and will display a menu.
+ * 
+ */
+int main(){
+    time_t t;
+    t = time(NULL);
+    struct tm tm = *localtime(&t);
+    printf("Welcome to the alarm clock! It is currently %d-%d-%d %d:%d:%d\n", tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
+    printf("Please enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit) \n");
+    selected = getchar();
+    checkInput();
     return 0;
 }
