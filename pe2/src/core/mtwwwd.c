65a9b1eed091c18eb5eb172d061977b6193c2041
Seran Shanmugathas <Seran@live.no>
2022-03-15 20:49:28 +0100

    :ambulance: (#15) Fixing bug with running server

--- pe2/src/core/mtwwwd.c
+++ pe2/src/core/mtwwwd.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <strings.h>
-#include <netinet/in.h>
+#include <string.h>
 #include <unistd.h>
+#include <netinet/in.h>
+#include <fcntl.h>
 
-#define PORT 6789
+#define PORT 8000
 #define MAXREQ (4096*10124)
 
 char buffer[MAXREQ], body[MAXREQ], msg[MAXREQ];
@@ -16,11 +15,12 @@ void error(const char *msg) {
     exit(1);
 }
 
-int main() {
-    int sockfd, newsockfd;
-    socklen_t clilen;
-    struct sockaddr_in serv_addr, cli_addr;
-    int n;
+//Socket headers for server and client
+int sockfd, newsockfd; 
+socklen_t clilen;
+struct sockaddr_in serv_addr, cli_addr;
+
+void initalize_server() {
 
     //Creates the socket
     sockfd = socket(PF_INET, SOCK_STREAM, 0);
@@ -33,16 +33,18 @@ int main() {
     bzero((char *) &serv_addr, sizeof(serv_addr));
 
     serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
     serv_addr.sin_port = htons(PORT);
 
     //Binds the socket to a address
-    if (bind(sockfd, (struct sockaddr * ) &serv_addr, sizeof(serv_addr) < 0))
+    if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
     {
         error("Something went wrong when binding the socket to address");
     }
     
     listen(sockfd, 5);
 
+    int n;
     while (1)
     {
         clilen = sizeof(cli_addr);
@@ -51,14 +53,14 @@ int main() {
 
         if (newsockfd < 0) 
         {
-            error("Somethin went wrong when acceptin a new socket");
+            error("Somethin went wrong when accepting a new socket");
         }
         
         bzero(buffer, sizeof(buffer));
         //Reads the HTTP request
         n = read(newsockfd, buffer, sizeof(buffer) - 1);
         
-        if (newsockfd < 0)
+        if (n < 0)
         {
             error("Something went wrong when reading from the socket");
         }
@@ -68,19 +70,15 @@ int main() {
             "<html>\n<body>\n"
             "<h1>Hello web browser</h1>\nYour request was\n"
             "<pre>%s</pre>\n"
-            "</body>\n</html>\n", 
-            buffer
-        );
+            "</body>\n</html>\n", buffer);
 
         snprintf(msg, sizeof (msg),
             "HTTP/1.0 200 OK\n"
             "Content-Type: text/html\n"
-            "Content-Length: %lu\n\n%s", 
-            strlen(body), body
-        );
+            "Content-Length: %d\n\n%s", (int) strlen(body), body);
 
         //Sends the response
-        n = write (newsockfd, msg, strlen(msg));
+        n = write(newsockfd, msg, strlen(msg));
 
         if (n < 0) 
         {
@@ -89,5 +87,10 @@ int main() {
 
         //Closes the connection
         close(newsockfd); 
-    }   
+    }
+    close(sockfd);   
+}
+
+int main(int argc, char* argv[]) {
+    initalize_server();
 }
