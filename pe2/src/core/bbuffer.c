131cbcef63fdd24304f90f4d01918fc51e8fca52
Seran Shanmugathas <Seran@live.no>
2022-03-16 16:50:13 +0100

    :seedling: issue #19 - pasting in methods from header file

---
+++ pe2/src/core/bbuffer.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "../../include/core/bbuffer.h"
+#include "../../include/core/sem.h"
+
+/**
+ * @brief check header file for instruction
+ * 
+ */
+typedef struct BNDBUF 
+{
+
+} BNDBUF;
+
+/**
+ * @brief check header file for instruction
+ * 
+ */
+BNDBUF *bb_init(unsigned int size) 
+{
+
+}
+
+/**
+ * @brief check header file for instruction
+ * 
+ */
+void bb_del(BNDBUF *bb)
+{
+
+}
+
+/**
+ * @brief check header file for instruction
+ * 
+ */
+int  bb_get(BNDBUF *bb)
+{
+
+}
+
+/**
+ * @brief check header file for instruction
+ * 
+ */
+void bb_add(BNDBUF *bb, int fd)
+{
+    
+}
