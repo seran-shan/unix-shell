accd0a3266f8297adbd8aa6ece68151d151c9456
Seran Shanmugathas <Seran@live.no>
2022-04-06 09:35:25 +0200

    :art: issue #24 - adding header file

---
+++ pe3/include/core/flush.h
@@ -0,0 +1,15 @@
+#ifndef ____FLUSH___H___
+#define ____FLUSH___H___
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "linkedlist.h"
+
+#define SIZE_OF(x) (sizeof(x) / sizeof(x[0]))
+
+#endif
